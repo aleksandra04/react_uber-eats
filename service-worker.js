a29c60e287aef5a50b612d25fe369b62eed999f3
Alexandra Boyko <Alexboyko04@gmail.com>
2020-01-16 00:48:49 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_uber-eats/precache-manifest.f800a86c9bf5c16cba59d9e462da7676.js"
+  "/react_uber-eats/precache-manifest.529538b276553d347c4f4845e4eaefc3.js"
 );
 
 self.addEventListener('message', (event) => {
